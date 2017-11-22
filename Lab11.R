77e408fd153de562a52038cb7b7ef63074e0b87f
ajaegge <andrea.jaegge@gmail.com>
2017-11-21 20:17:16 -0600

    Set GOM study area

--- Lab11.R
+++ Lab11.R
@@ -4,3 +4,8 @@ install.packages("reshape2")
 library(ncdf4)
 library(reshape2)
 
+#set study area
+lonmax <- 30.7 #top northern most coordinate
+lonmin <-17.9 #bott0m southern coordinate
+latmax <- -98.2 #left eastern coordinate
+latmin <- -80.9#right western coordinate
