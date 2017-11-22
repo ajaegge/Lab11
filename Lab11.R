59cbb3c8f903e0c8fb4cab6ba89de45198aca2b6
ajaegge <andrea.jaegge@gmail.com>
2017-11-22 10:17:01 -0600

    trying to run all files & write txt file

--- Lab11.R
+++ Lab11.R
@@ -4,6 +4,8 @@ install.packages("reshape2")
 library(ncdf4)
 library(reshape2)
 
+setwd("/Users/andreajaegge/Desktop/Lab11/chlor_a")
+
 #set study area
 lonmax <- -98.2 #top northern most coordinate
 lonmin <- -80.9 #bott0m southern coordinate
@@ -14,12 +16,14 @@ latmin <- 17.9#right western coordinate
 var <-"chlor_a"
 
 #list netCDF files
-f <- list.files("/Users/andreajaegge/Desktop/Lab11/chlor_a",pattern=".nc",full.names=F) #What pattern can you use to identify all the netCDF files?
+f <- list.files(pattern=".nc",full.names=F) #What pattern can you use to identify all the netCDF files?
+
+f
 
 #d <- plyr::adply(f, 1, function(file) {
 
 #open netCDF file
-data<-nc_open(file.choose('A2017291.L3m_DAY_CHL_chlor_a_4km.nc'), write = FALSE)
+data<-nc_open(file)
 
 # extract data
 lon<-ncvar_get(data,"lon")
@@ -27,7 +31,7 @@ lat<- ncvar_get(data, "lat")# get the latitude
 tmp.array <- ncvar_get(data, var)
 dunits <- ncatt_get(data, var, "units")$value
 fillvalue <- -999 #set the fill value for cells with no data
-  
+
 dim(tmp.array)
 
 # remove the missing data
@@ -53,6 +57,19 @@ day <- format(as.Date(datemean, "%Y-%m-%dT%H:%M:%OSZ"), "%d")# get the day
 dat.varSA<-data.frame(rep(as.integer(year,nrow(dat.varSAtmp))), rep(as.integer(month,nrow(dat.varSAtmp))), 
                       rep(as.integer(day,nrow(dat.varSAtmp))), dat.varSAtmp, rep(dunits,nrow(dat.varSAtmp)), 
                       rep(var, nrow(dat.varSAtmp)))
-
 names(dat.varSA)<-c("year","month","day","lon","value","unit","var")
 
+# close connection
+nc_close(data)
+
+return(dat.varSA)
+
+}, .progress="text", .inform = T)
+
+d <- d[,-1]
+
+#save csv
+
+summary(d)
+write.table(d, "chl_a.txt", sep="\t", row.names=FALSE)
+write.table(summary(d), "chl_a.txt", sep="\t", row.names=FALSE)
